16598021c1af33f4e46a8dc905ec654aa346bc62
0x0u <m0zurillex@gmail.com>
2020-11-27 14:01:26 +0900

    :+1: デバッグをやりやすくした

--- main.py
+++ main.py
@@ -9,23 +9,16 @@ from linebot.exceptions import InvalidSignatureError
 import flex_message_template
 
 
-# firestoreの初期化（debug）
-# from firebase_admin import credentials
-# cred = credentials.Certificate('serviceAccountKey.json')
-# firebase_admin.initialize_app(cred)
-# db = firestore.client()
+debug = True
 
-# firestoreの初期化（デバッグ時は以下2行をコメントアウト）
-firebase_admin.initialize_app()
-db = firestore.client()
+# flaskのインスタンスの作成
+app = Flask(__name__)
 
 # line-messaging-apiの初期化
 LINE_CHANNEL_ACCESS_TOKEN = os.environ.get('LINE_CHANNEL_ACCESS_TOKEN')
 LINE_CHANNEL_SECRET = os.environ.get('LINE_CHANNEL_SECRET')
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
-
-app = Flask(__name__)
 
 room_ids = {
     '学習席（有線LAN有）': 0,
@@ -35,6 +28,17 @@ room_ids = {
     'グループ学習室': 4,
     'ティーンズ学習室': 5
 }
+
+
+def initialize_firestore():
+    if debug:
+        from firebase_admin import credentials
+        cred = credentials.Certificate('serviceAccountKey.json')
+        firebase_admin.initialize_app(cred)
+        return firestore.client()
+    else:
+        firebase_admin.initialize_app()
+        return firestore.client()
 
 
 def get_room_data():
@@ -50,7 +54,8 @@ def get_room_data():
     if doc.exists:
         rooms = doc.to_dict().get(time)
         if not rooms:
-            rooms = doc.to_dict().get((now - timedelta(minutes=1)).strftime('%H%M'))
+            before = (now - timedelta(minutes=1)).strftime('%H%M')
+            rooms = doc.to_dict().get(before)
 
     return rooms
 
@@ -83,13 +88,19 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+
+    print(event.source)
+
     message = event.message.text
     flex_message = create_message(message)
     line_bot_api.reply_message(event.reply_token, messages=flex_message)
 
 
 if __name__ == '__main__':
-    app.run(threaded=True)
 
-    # debug
-    # app.run(host='0.0.0.0', port=8000, threaded=True, debug=True)
+    db = initialize_firestore()
+
+    if debug:
+        app.run(host='0.0.0.0', port=8000, threaded=True, debug=True)
+    else:
+        app.run(threaded=True)

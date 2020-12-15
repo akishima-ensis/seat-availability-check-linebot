b69197fdf9dabebec8b948e1cbc043d998c1b834
0x0u <m0zurillex@gmail.com>
2020-12-15 14:38:06 +0900

    :+1: ゼロ埋め処理をする

--- src/message.py
+++ src/message.py
@@ -49,7 +49,7 @@ def create_new_reserve_notice_message(room_name, user_id):
                 if room_data['seats_num'] == 0:
                     reserve_time = firebase.reserve_notice(user_id, room_name)
                     reserve_time_delta = reserve_time + datetime.timedelta(hours=1)
-                    notice_time = f'{reserve_time.hour}:{reserve_time.minute} 〜 {reserve_time_delta.hour}:{reserve_time_delta.minute}'
+                    notice_time = f'{str(reserve_time.hour).zfill(2)}:{str(reserve_time.minute).zfill(2)} 〜 {str(reserve_time_delta.hour).zfill(2)}:{str(reserve_time_delta.minute).zfill(2)}'
                     return flex_message_template.done_reservation_message(room_name, notice_time)
                 else:
                     return TextSendMessage(text=f'{room_name}は空席があります。')

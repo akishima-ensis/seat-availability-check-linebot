139ee7a505610973c9155a00ea38fb7f8376cc4d
0x0u <m0zurillex@gmail.com>
2021-01-29 18:53:07 +0900

    :recycle: アノテーションの追加

--- src/firebase.py
+++ src/firebase.py
@@ -1,13 +1,26 @@
-from src import db
+from typing import Dict, Union
 from datetime import datetime, timedelta, timezone
 
+from src import db
 
-def get_time():
+
+def get_time() -> datetime:
+    """
+    現在時刻の取得（JST）
+    Returns:
+        datetime
+    """
     jst = timezone(timedelta(hours=+9), 'JST')
     return datetime.now(jst)
 
 
-def get_rooms_data():
+def get_rooms_data() -> Dict:
+    """
+    最新の空席情報の取得
+
+    Returns:
+        dict: 空席情報
+    """
     now = get_time()
     date = now.strftime('%Y%m%d')
     time = now.strftime('%H%M')
@@ -21,19 +34,34 @@ def get_rooms_data():
     return rooms_data
 
 
-def check_reserve_notice(user_id):
+def get_reserved_room(user_id: str) -> Union[str, None]:
+    """
+    予約が存在するかの確認
+
+    Args:
+        user_id(str): LINEのユーザーID
+
+    Returns:
+        dict:
+    """
     user_ref = db.collection('users').document(user_id).get()
     if user_ref.exists:
         data = user_ref.to_dict()
         if data['reserved']:
-            return {'reserved': True, 'name': data['room_name']}
-        else:
-            return {'reserved': False}
-    else:
-        return {'reserved': False}
+            return data['room_name']
 
 
-def reserve_notice(user_id, room_name):
+def reserve_notice(user_id: str, room_name: str) -> datetime:
+    """
+    空席通知予約
+
+    Args:
+        user_id(str): LINEのユーザーID
+        room_name(str): 学習室名
+
+    Returns:
+        datetime: 予約時間
+    """
     now = get_time()
     users_ref = db.collection('users').document(user_id)
     data = {
